73f7f6a55cdd195b872db1d7938f10aaa0499c50
Kajigga Dev <kajigga@gmail.com>
2019-02-28 11:29:08 -0700

    how about now?

--- first-bite/assets/install_salt.sh
+++ first-bite/assets/install_salt.sh
@@ -4,11 +4,10 @@ wget -O - https://repo.saltstack.com/py3/ubuntu/16.04/amd64/2019.2/SALTSTACK-GPG
 echo "deb http://repo.saltstack.com/py3/ubuntu/16.04/amd64/2019.2 xenial main" > /etc/apt/sources.list.d/saltstack.list
 apt-get update
 apt-get install -y salt-master salt-minion --allow-unauthenticated
-sleep 1
 service salt-master start
 echo "master: localhost" > /etc/salt/minion.d/master.conf
 echo "id: saltmaster" > /etc/salt/minion.d/id.conf
-service salt-minion restart
+service salt-minion start
 echo "accepting minion key"
 until salt-key -l acc | grep saltmaster; do
     sleep 1
