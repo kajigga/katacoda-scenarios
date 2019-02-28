80b7b28476158cfd2f1149098b2be73cf90be648
Kajigga Dev <kajigga@gmail.com>
2019-02-28 11:33:04 -0700

    and now?

--- first-bite/assets/install_salt.sh
+++ first-bite/assets/install_salt.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 wget -O - https://repo.saltstack.com/py3/ubuntu/16.04/amd64/2019.2/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
 echo "deb http://repo.saltstack.com/py3/ubuntu/16.04/amd64/2019.2 xenial main" > /etc/apt/sources.list.d/saltstack.list
---
+++ first-bite/assets/launch_install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sh ~/launch_install.sh
