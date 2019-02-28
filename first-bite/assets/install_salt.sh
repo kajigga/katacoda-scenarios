bae7d8a3cf2c3d28819c75a0eabd228fb0b60888
Kajigga Dev <kajigga@gmail.com>
2019-02-28 11:52:51 -0700

    make grep output quiet

--- first-bite/assets/install_salt.sh
+++ first-bite/assets/install_salt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -O - https://repo.saltstack.com/py3/ubuntu/16.04/amd64/2019.2/SALTSTACK-GPG-KEY.pub | apt-key add -
+curl https://repo.saltstack.com/py3/ubuntu/16.04/amd64/2019.2/SALTSTACK-GPG-KEY.pub | apt-key add -
 echo "deb http://repo.saltstack.com/py3/ubuntu/16.04/amd64/2019.2 xenial main" > /etc/apt/sources.list.d/saltstack.list
 apt-get update
 apt-get install -y salt-master salt-minion --allow-unauthenticated
@@ -9,7 +9,7 @@ echo "master: localhost" > /etc/salt/minion.d/master.conf
 echo "id: saltmaster" > /etc/salt/minion.d/id.conf
 service salt-minion restart
 echo "accepting minion key"
-until salt-key -l acc | grep saltmaster; do
+until salt-key -l acc | grep -q -s saltmaster; do
     sleep 1
     salt-key -a saltmaster -y
 done
