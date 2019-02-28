4090343ba729e85cb5cd414df6129cd516276b36
Kajigga Dev <kajigga@gmail.com>
2019-02-28 11:27:40 -0700

    The whole script isn't running, maybe this will fix it

--- first-bite/assets/install_salt.sh
+++ first-bite/assets/install_salt.sh
@@ -1,26 +1,17 @@
 #!/bin/sh
 
-
 wget -O - https://repo.saltstack.com/py3/ubuntu/16.04/amd64/2019.2/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
-
 echo "deb http://repo.saltstack.com/py3/ubuntu/16.04/amd64/2019.2 xenial main" > /etc/apt/sources.list.d/saltstack.list
-
 apt-get update
 apt-get install -y salt-master salt-minion --allow-unauthenticated
-
-sleep 2
-
+sleep 1
 service salt-master start
-
 echo "master: localhost" > /etc/salt/minion.d/master.conf
 echo "id: saltmaster" > /etc/salt/minion.d/id.conf
-
 service salt-minion restart
-
 echo "accepting minion key"
 until salt-key -l acc | grep saltmaster; do
     sleep 1
     salt-key -a saltmaster -y
 done
-
 
