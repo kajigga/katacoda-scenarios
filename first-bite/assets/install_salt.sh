071cd3ae41b813c64efc8517c7c2bb6c1402cb22
Kajigga Dev <kajigga@gmail.com>
2019-03-04 14:17:44 -0700

    exit script, now using docker

--- first-bite/assets/install_salt.sh
+++ first-bite/assets/install_salt.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Add the current saltstack repo to APT
+exit
 curl https://repo.saltstack.com/py3/ubuntu/16.04/amd64/2019.2/SALTSTACK-GPG-KEY.pub | apt-key add -
 echo "deb http://repo.saltstack.com/py3/ubuntu/16.04/amd64/2019.2 xenial main" > /etc/apt/sources.list.d/saltstack.list
 apt-get update
