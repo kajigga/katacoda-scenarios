2f8cee48215e7c312195304ca2cd400b6d442ae4
Kajigga Dev <kajigga@gmail.com>
2019-03-05 10:06:29 -0700

    updated dotfiles

---
+++ first-bite/assets/dotfiles/.vimrc
@@ -0,0 +1,11 @@
+set nocompatible
+set modeline
+set nu
+set nowrap
+syntax on
+filetype indent on
+
+tabstop=8
+expandtab
+shiftwidth=4
+softtabstop=4
--- first-bite/assets/install_salt.sh
+++ first-bite/assets/install_salt.sh
@@ -2,11 +2,14 @@
 
 # Add the current saltstack repo to APT
 
+mv ~/dotfiles/* ~/
+source ~/.bashrc
+
+exit
 cd ~/tutorial/docker
 docker-compose build
 docker-compose up -d
 
-exit
 
 curl https://repo.saltstack.com/py3/ubuntu/16.04/amd64/2019.2/SALTSTACK-GPG-KEY.pub | apt-key add -
 echo "deb http://repo.saltstack.com/py3/ubuntu/16.04/amd64/2019.2 xenial main" > /etc/apt/sources.list.d/saltstack.list
