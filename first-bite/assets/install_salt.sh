34e4195097332463f0f72d61e3660046981ce6ac
Kajigga Dev <kajigga@gmail.com>
2019-03-06 09:25:54 -0700

    ignore pgddata folder

--- first-bite/assets/install_salt.sh
+++ first-bite/assets/install_salt.sh
@@ -7,7 +7,7 @@
 # echo "."
 # done
 echo "waiting for dotfiles"
-until [ -f ~/dotfiles ]; do
+until [ -d ~/dotfiles ]; do
 echo "."
 done
 
@@ -23,30 +23,29 @@ cd ~/docker
 # container starting
 mkdir pgdata
 
-exit
 
-docker-compose up -d
+# docker-compose up -d
 
-curl https://repo.saltstack.com/py3/ubuntu/16.04/amd64/2019.2/SALTSTACK-GPG-KEY.pub | apt-key add -
-echo "deb http://repo.saltstack.com/py3/ubuntu/16.04/amd64/2019.2 xenial main" > /etc/apt/sources.list.d/saltstack.list
-apt-get update
+# curl https://repo.saltstack.com/py3/ubuntu/16.04/amd64/2019.2/SALTSTACK-GPG-KEY.pub | apt-key add -
+# echo "deb http://repo.saltstack.com/py3/ubuntu/16.04/amd64/2019.2 xenial main" > /etc/apt/sources.list.d/saltstack.list
+# apt-get update
 
-# Install salt-master and salt-minion
-apt-get install -y salt-master salt-minion
+# # Install salt-master and salt-minion
+# apt-get install -y salt-master salt-minion
 
-# restart the salt-master
-service salt-master restart
+# # restart the salt-master
+# service salt-master restart
 
-# add some configurations to the salt-minion
-echo "master: localhost" > /etc/salt/minion.d/master.conf
-echo "id: saltmaster" > /etc/salt/minion.d/id.conf
+# # add some configurations to the salt-minion
+# echo "master: localhost" > /etc/salt/minion.d/master.conf
+# echo "id: saltmaster" > /etc/salt/minion.d/id.conf
 
-# restart the salt-minion
-service salt-minion restart
+# # restart the salt-minion
+# service salt-minion restart
 
-# wait until the minion key appears then accept it
-echo "accepting minion key"
-until salt-key -l acc | grep -q -s saltmaster; do
-    sleep 1
-    salt-key -a saltmaster -y
-done
+# # wait until the minion key appears then accept it
+# echo "accepting minion key"
+# until salt-key -l acc | grep -q -s saltmaster; do
+#     sleep 1
+#     salt-key -a saltmaster -y
+# done
